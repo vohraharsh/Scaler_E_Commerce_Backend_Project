7aa88f6577d43825ea81a4fa1d9ce52c7266575b
vohraharsh <harshvohra995@gmail.com>
2025-04-05 13:40:02 +0530

    Added DB with the product service

--- src/main/java/com/vohraharsh/productservice/models/Category.java
+++ src/main/java/com/vohraharsh/productservice/models/Category.java
@@ -1,12 +1,19 @@
 package com.vohraharsh.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 }
--- src/main/java/com/vohraharsh/productservice/models/Product.java
+++ src/main/java/com/vohraharsh/productservice/models/Product.java
@@ -1,17 +1,24 @@
 package com.vohraharsh.productservice.models;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private float price;
     private String description;
     private String imageUrl;
+
+    @ManyToOne
     private Category category;
 
 }
